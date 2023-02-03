ffe1bf640ee7a1e545fe3894bd6908b55a6d6e05
Race Osuna <raceosuna@gmail.com>
2023-02-02 20:26:55 -0700

    Add functions to pass meals tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,49 @@
-function nameMenuItem() {
-
+function nameMenuItem(food) {
+var menuItemName = `Delicious ${food}`
+  return menuItemName
 }
 
+function createMenuItem(menuItemName, num, mealType) {
+  menuItem = {
+    name: menuItemName,
+    price: num,
+    type: mealType
+  }
+  return menuItem
+}
+
+function addIngredients(addFood, ingredients) {
+if (!ingredients.includes(addFood)) {
+  ingredients.push(addFood)
+}
+}
+
+function formatPrice(initialPrice) {
+var formattedPrice = (`$${initialPrice}`);
+return formattedPrice;
+ }
+
+function decreasePrice() {
+var decreasedPrice = (menuItem.price * 0.90)
+return decreasedPrice
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+  return recipe
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
