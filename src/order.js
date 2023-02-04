1197d996db8df371a1b34c9cc8f9a26badffbcc2
Race Osuna <raceosuna@gmail.com>
2023-02-03 21:34:51 -0700

    Add functions to pass tests in orders

--- src/order.js
+++ src/order.js
@@ -1,7 +1,39 @@
+function takeOrder(order, array) {
+  if (array.length < 3)
+  array.push(order);
+}  
+  
+function refundOrder(num, array) {
+  for(var i = 0; i < array.length; i++) {
+ if (array[i].orderNumber === num) {
+   return array.splice([i], 1);
+      }
+  }
+}
 
+function listItems(array) {
+  var items = []
+  for (var i = 0; i < array.length; i++) {
+    items.push(array[i].item);
+  }
+  return items.join(', ')
+  }
+
+  function searchOrder(array, food) {
+    order = false
+    for (var i = 0; i < array.length; i++) {
+    if (array[i].item === food) {
+     order = true
+      } 
+    }
+      return order
+  }
+  
+  
+  
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
